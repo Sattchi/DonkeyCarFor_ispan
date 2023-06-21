4e79bdbbf5088f67bfa0f0dca09949942d43c747
ohayowu314 <48574669+ohayowu314@users.noreply.github.com>
2023-06-21 11:17:27 +0800

    Update workSpace_donkey_web 修改 central 網頁 修改 格式 將主控台改成 iframe 加入一些 json資料集

--- web_car_DaRen/index.js
+++ web_car_DaRen/index.js
@@ -28,16 +28,29 @@ const io = require('socket.io');
 const spawn = require('child_process').spawn;
 
 // const fileNames = ["runForever1.py", "runForever2.py"]
-const fileNames = ["manage.py", "manage.py", "manage.py", "manage.py", "manage.py", "manage.py", "manage.py"]
-const fileParas = [["drive", "--model", "./models/mypilot_forward_only.h5"],
-["drive", "--model", "./models/mypilot_backward.h5"],
-["drive", "--model", "./models/mypilot_reverse.h5"],
-["drive", "--model", "./models/mypilot_backward_reverse.h5"],
-["drive", "--model", "./models/mypilot_yellow_3800.h5"],
-["drive", "--model", "./models/mypilot_yellow_6500.h5"],
-["drive", "--model", "./models/mypilot_forward_only.h5"]];
-const fileOptions = [{ cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }];
-const fileChins = ["順時針前進", "順時針倒車","逆時針前進", "逆時針倒車", "路邊停車3800", "路邊停車6500"];
+// const fileNames = ["manage.py", "manage.py", "manage.py", "manage.py", "manage.py", "manage.py", "manage.py"]
+// const fileParas = [["drive", "--model", "./models/mypilot_forward_only.h5"],
+// ["drive", "--model", "./models/mypilot_backward.h5"],
+// ["drive", "--model", "./models/mypilot_reverse.h5"],
+// ["drive", "--model", "./models/mypilot_backward_reverse.h5"],
+// ["drive", "--model", "./models/mypilot_yellow_3800.h5"],
+// ["drive", "--model", "./models/mypilot_yellow_6500.h5"],
+// ["drive", "--model", "./models/mypilot_forward_only.h5"]];
+// const fileOptions = [{ cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }, { cwd: "/home/pi/mycar2" }];
+// const fileChins = ["順時針前進", "順時針倒車","逆時針前進", "逆時針倒車", "路邊停車3800", "路邊停車6500"];
+
+
+function readTextFile(file, callback) {
+    fs.readFile(file, function (err, text) {
+        callback(text);
+    });
+}
+
+readTextFile("www/json/autoModel.json", function(text){
+    modelData = JSON.parse(text);
+    console.log(modelData);
+    fileChins = Object.values(modelData).map(item => item.chinName);
+});
 
 app.get("/", function (req, res, next) {
     res.render('index', { "title": "控制台", "fileChins": fileChins });
@@ -54,13 +67,12 @@ var sio = io.listen(server);
 var subprocess = null;
 
 sio.on('connection', function (socket) {
-    socket.emit("fileUpload", fileChins);
-
-    socket.on('disconnect', function () {
-        console.log('Bye, bye!');
-        stopProcess();
-        process.exit();
-    });
+    // socket.emit("fileUpload", fileChins);
+    // socket.on('disconnect', function () {
+    //     console.log('Bye, bye!');
+    //     stopProcess();
+    //     process.exit();
+    // });
     socket.on('prc', function (num) {
         doProcess(num)
     });
@@ -79,10 +91,14 @@ process.on("exit", function () {
 
 function doProcess(num) {
     stopProcess();
-    const fileName = fileNames[num];
-    const filePara = fileParas[num];
+    // const fileName = fileNames[num];
+    // const filePara = fileParas[num];
+    // const para = (!filePara) ? [fileName] : [fileName].concat(filePara);
+    // const fileOption = fileOptions[num];
+    const fileName = modelData[num].fileName;
+    const filePara = modelData[num].para;
     const para = (!filePara) ? [fileName] : [fileName].concat(filePara);
-    const fileOption = fileOptions[num];
+    const fileOption = modelData[num].option;
     console.log("想執行 Python 檔案: " + fileName);
     console.log("必選參數: " + para);
     console.log("可選參數: ");
